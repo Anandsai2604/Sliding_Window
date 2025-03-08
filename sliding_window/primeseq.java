5f1ec8b5b5bf635464dfce8acb88c2d36c3b6001
Anand <anand@gmail.com>
2025-03-08 09:46:41 +0530

    Done

---
+++ sliding_window/primeseq.java
@@ -0,0 +1,92 @@
+package sliding_window;
+
+// In a distant galaxy, there exists an ancient space station covered in a vast 
+// array of digital codes. These codes are believed to hold the key to unlocking 
+// powerful alien technology. The interstellar explorers call these codes "prime 
+// sequences."
+
+// The prime-sequence beauty of the digital code is defined as the number of prime 
+// sequences that meet the following conditions:
+//     -The prime sequence has a length of k.
+//     -The prime sequence is a divisor of the entire digital code.
+//     -The prime sequence is a prime number.
+
+// Given the digital code on the space station, represented as an integer code, and
+// the length of the prime sequences k, return the prime-sequence beauty of the code.
+
+// Note:
+// -----
+// Leading zeros in prime sequences are allowed.
+// 0 is not a divisor of any value.
+// A prime sequence is a contiguous sequence of characters in the main digital code.
+
+// Input Format:
+// -------------
+// Line-1: space separated String and integer, code and K
+
+// Output Format:
+// -------------
+// An integer, the prime-sequence beauty of the code.
+
+
+// Sample Input:
+// -------------
+// 239246 2 
+
+// Sample Output:
+// --------------
+// 1 
+
+// Explanation:
+// ------------
+// The following are the prime sequences of length k:
+//     "23" from "239246": 23 is a divisor of 239246 and is a prime number.
+//     "39" from "239246": 39 is not a divisor.
+//     "92" from "239246": 92 is not a divisor.
+//     "24" from "239246": 24 is is not a divisor 239246.
+//     "46" from "239246": 46 is a divisor of 239246 but is not a prime number.
+//     Therefore, the prime-sequence beauty is 1.
+
+// Sample Input:
+// -------------
+// 24224 1
+
+// Sample Output:
+// --------------
+// 3
+
+import java.util.Scanner;
+
+public class primeseq {
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static int primeSequenceBeauty(String code, int k) {
+        int beauty = 0;
+        long fullCode = Long.parseLong(code);
+        int i = 0;
+        
+        while (i + k <= code.length()) {
+            int num = Integer.parseInt(code.substring(i, i + k));
+            if (num != 0 && fullCode % num == 0 && isPrime(num)) {
+                beauty++;
+            }
+            i++;
+        }
+        
+        return beauty;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String code = sc.next();
+        int k = sc.nextInt();
+        System.out.println(primeSequenceBeauty(code, k));
+        sc.close();
+    }
+}
